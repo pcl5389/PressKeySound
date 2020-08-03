b5882dc0328bcdb6841ee545b097a12ecbef92b1
宁 李 <pcl5389@163.com>
2020-08-03 12:17:54 +0800

    增加自动编译

---
+++ Beat/frmLoading.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace Beat
+{
+    public partial class frmLoading : Form
+    {
+        public frmLoading()
+        {
+            InitializeComponent();
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+    }
+}
---
+++ Beat/lib/NgenInstaller.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Beat.lib
+{
+    [RunInstaller(true)]
+    public partial class NgenInstaller// : System.Configuration.Install.Installer
+    {
+        public enum InstallTypes
+        {
+            Install,
+            Uninstall
+        }
+
+        public void NgenFile(InstallTypes options)
+        {
+            string envDir = RuntimeEnvironment.GetRuntimeDirectory();
+            string ngenPath = Path.Combine(envDir, "ngen.exe");
+            string exePath = Application.ExecutablePath;
+            string argument = (options == InstallTypes.Install ? "install" : "uninstall") + " \"" + exePath + "\"";
+
+            Process ngenProcess = new Process();
+            ngenProcess.StartInfo.FileName = ngenPath;
+            ngenProcess.StartInfo.Arguments = argument;
+            ngenProcess.StartInfo.CreateNoWindow = true;
+            ngenProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            ngenProcess.Start();
+
+            ngenProcess.WaitForExit();
+        }
+    }
+}
